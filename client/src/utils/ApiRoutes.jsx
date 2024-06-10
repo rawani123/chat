67955616e19209ed91c7435c7d37cab491cf89b5
kashish567 <kashishbhanushali7@gmail.com>
2024-06-10 16:26:18 +0530

    components

--- client/src/utils/ApiRoutes.jsx
+++ client/src/utils/ApiRoutes.jsx
@@ -4,3 +4,8 @@ export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 
 export const SetAvatarRoute = `${host}/api/auth/setAvatar`;
+
+export const logoutRoute = `${host}/api/auth/logout`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;
